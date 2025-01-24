1516ef4c189fdc4bff9cafe88514bba62f852a05
Fatih Emre Ertekin <ertekin_ertekin_74@hotmail.com>
2025-01-25 01:53:49 +0300

    first commit: project is uploaded

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+function App() {
+  
+
+  return (
+    <main>
+      <div id="gam-container">
+        <ol id="players">
+          <li></li>
+          <li></li>
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
+  )
+}
+
+export default App
