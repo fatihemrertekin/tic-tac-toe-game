493c226952d238e945cf690d4f78b4dea90d84cf
Fatih Emre Ertekin <ertekin_ertekin_74@hotmail.com>
2025-01-27 16:10:34 +0300

    some state lifting operations were performed

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,28 @@ import { useState } from "react";
 
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 
 function App() {
-  const [activePlayer,setActivePlayer] = useState('X');
+  const [gameTurs, setGameTurns] = useState([]);
+  const [activePlayer, setActivePlayer] = useState('X');
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((curActivePlayer) => curActivePlayer === "X" ? "O" : "X");
+    setGameTurns(prevTurns => {
+      let currentPlayer = "X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+
+      const updatedTurns = [
+        {square: {row: rowIndex, col: colIndex}, player: currentPlayer},
+        ...prevTurns
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
@@ -19,6 +35,7 @@ function App() {
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
       </div>
+      <Log />
     </main>
   )
 }
